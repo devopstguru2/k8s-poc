9751c017e69029c02a4957250df3bf959ca53236
root <root@jenkins.us-west4-b.c.impactful-mode-356614.internal>
2022-08-07 15:55:37 +0000

    Added jenkins installations script

---
+++ scripts/jenkins-installation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+// Following script will do the jenkins installations
+
+sudo apt-get update -y
+sudo apt-get install openjdk-8-jdk -y
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+sudo systemctl start jenkins
