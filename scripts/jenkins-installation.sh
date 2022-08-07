4370f9416b7bc8cdfc7e1f17d723a203007d9b51
devopstechnoguru2 <devopstechnoguru2@gmail.com>
2022-08-07 18:44:17 +0000

    updating jenkinsfile

--- scripts/jenkins-installation.sh
+++ scripts/jenkins-installation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-// Following 1script will do the jenkins installations
+// Following script will do the jenkins installations
 
 sudo apt-get update -y
 sudo apt-get install openjdk-8-jdk -y
